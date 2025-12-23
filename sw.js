dbdb5e40415dc8c9c805b38516f12d453eca28ae
wya-tt <wang631485811@gmail.com>
2025-12-22 21:37:59 -0500

    Site updated: 2025-12-22 21:37:57

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1766453949221";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1766457459258";const preCache = ["/images/taichi.png","/images/mybanner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
